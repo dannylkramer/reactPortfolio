1807e82de42db4d239762fbb02c1d531d0fbcd5e
dannywashere <dannylkramer@icloud.com>
2024-08-05 16:25:54 -0500

    styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,13 @@ import '../styles/Footer.css';
 const Footer = () => {
   return (
     <footer>
-                <li><a href="https://github.com/dannylkramer"> Github</a></li>
-                <li><a href="mailto: dannylkramer@icloud.com"> dannylkramer@icloud.com </a></li>
-                <li><a href="tel:+16306406342">(630) 640-6342</a></li>
-                <li><a href="https://twitter.com/dannykwashere"> Twitter </a></li>
-                <li><a href="https://letterboxd.com/dannywashere/"> Letterboxd </a></li>
+      <a href="https://github.com/dannylkramer"> Github</a>
+      <a href="mailto: dannylkramer@icloud.com"> dannylkramer@icloud.com </a>
+      <a href="tel:+16306406342">(630) 640-6342</a>
+      <a href="https://twitter.com/dannykwashere"> Twitter </a>
+      <a href="https://letterboxd.com/dannywashere/"> Letterboxd </a>
     </footer>
   );
 };
 
 export default Footer;
-              
