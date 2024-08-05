5fdf443b0007f8bb4cd008360a1596fa02f6dff0
dannywashere <dannylkramer@icloud.com>
2024-08-05 14:05:39 -0500

    app, main, and index.html

---
+++ README.md
@@ -0,0 +1,3 @@
+correct paths 
+add images and links to projects
+add pdf of resume
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/index.css'; // Import global styles if you have any
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
